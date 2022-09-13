d00d5e8cdfadc19adc99dcdd52e9fbf9ad2eb951
Augusto Souza <augusto_souza@edu.unifil.br>
2022-09-13 11:20:43 -0300

    :finalizado: finalizado jogo da memória

--- src/templates/App/index.tsx
+++ src/templates/App/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { CardProps } from "../../components/Card";
 import { Grid } from "../../components/Grid";
 import { cards } from "../../data/cards";
@@ -9,13 +9,43 @@ export function App() {
   const [stateCards, setStateCards] = useState(() =>
     duplicateRegenerateSortArray<CardProps>(cards)
   );
+  const [matches, setMatches] = useState(0);
+  const [moves, setMoves] = useState(0);
+
+  const firstCard = useRef<CardProps | null>(null);
+  const secondCard = useRef<CardProps | null>(null);
+  const unflip = useRef<boolean>(false);
+
   function handleClick(id: string) {
     const newStateCards = stateCards.map((card) => {
       if (card.id !== id) return card;
 
       if (card.flipped) return card;
 
+      if (unflip.current && firstCard.current && secondCard.current) {
+        firstCard.current.flipped = false;
+        secondCard.current.flipped = false;
+        firstCard.current = null;
+        secondCard.current = null;
+        unflip.current = false;
+      }
+
       card.flipped = true;
+
+      if (!firstCard.current) {
+        firstCard.current = card;
+      } else if (!secondCard.current) {
+        secondCard.current = card;
+        setMoves((m) => m + 1);
+      }
+
+      if (firstCard.current.emoji === secondCard.current?.emoji) {
+        firstCard.current = null;
+        secondCard.current = null;
+        setMatches((mt) => mt + 1);
+      } else {
+        unflip.current = true;
+      }
 
       return card;
     });
@@ -23,8 +53,24 @@ export function App() {
     setStateCards(newStateCards);
   }
 
+  function handleResetClick() {
+    setStateCards(duplicateRegenerateSortArray(cards));
+    firstCard.current = null;
+    secondCard.current = null;
+    unflip.current = false;
+    setMoves(0);
+    setMatches(0);
+  }
+
   return (
     <div className="App">
+      <div className="text">
+        <h1>Jogo da mémoria</h1>
+        <p>
+          Movimentos: {moves} | Acertos: {matches}{" "}
+          <button onClick={handleResetClick}>Reiniciar</button>
+        </p>
+      </div>
       <Grid cards={stateCards} handleClick={handleClick} />
     </div>
   );
